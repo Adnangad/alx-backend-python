a7da82c99e18ad5c48f529256cd201eaf1c9aedb
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 09:12:51 +0000

    1-concatenating strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that concatenates 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Args:
+    str1: string 1
+    str2: string 2
+    """
+    return str1 + str2
