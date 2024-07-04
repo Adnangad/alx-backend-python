6e73cb4f4e59877457bab84c9a41508d5e9d6ee2
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 09:39:49 +0000

    5-adding a list of float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module sums the elements in a float list.
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Args:
+    input_list: a list of floats.
+    """
+    return sum(input_list)
