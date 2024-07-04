ea027a6888eeef1843de7d90629461763e058513
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 09:24:35 +0000

    2-floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that floors a float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Args:
+    n: a float
+    """
+    return math.floor(n)
