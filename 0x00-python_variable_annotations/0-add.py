07fcd13431c6fe8dd033765614166411186fbc59
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 09:06:41 +0000

    0-adding function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This mdule contains a function that adds two args.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+    a: a float
+    b: a float
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This project contains code on python variable annotations
