75ad38d3207cb7b7f74188d5880805319a02be07
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 10:57:01 +0000

    8-fixing possible errors

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,15 +5,17 @@ This module contains a function that returns a callable object.
 from typing import Callable
 
 
+def multiplier(x: float) -> float:
+    """
+    Args:
+    x: the arg to be multiplied
+    """
+    return x * x
+
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Args:
     multiplier: the float to be multiplied.
     """
-    def multiplier(x: float) -> float:
-        """
-        Args:
-        x: the arg to be multiplied
-        """
-        return x * x
     return multiplier
