2e31452b5c8a36feea00f249496bd01b0a2275b6
Adnangad <gardobuyaadnan@gmail.com>
2024-07-04 11:07:45 +0000

    fixed pycodestyle errors

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,15 +2,12 @@
 """
 This module sums the elements in a mixed int and float list.
 """
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Args:
     mxd_lst: a list of floats.
     """
-    a = 0.00
-    for i in mxd_lst:
-        a = a + i
-    return a
+    return sum(mxd_lst)
