a710240538b602f400faf2f67ffa4fc8a9053efb
Adnangad <gardobuyaadnan@gmail.com>
2024-07-09 10:45:09 +0000

    1-async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,15 @@
 This module contains a fnction that implements an async comprehension
 """
 import asyncio
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Returns a list.
     """
-    rez = [i async for i in async_generator()]
+    rez: List[float] = [i async for i in async_generator()]
     return rez
