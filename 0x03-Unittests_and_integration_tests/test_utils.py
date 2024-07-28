c92c582b84e9284fd180541cc90cd0b38a93ff2c
Adnangad <gardobuyaadnan@gmail.com>
2024-07-28 13:15:43 +0300

    added doczz

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,16 +6,13 @@ from utils import access_nested_map
 
 
 class TestUtils(unittest.TestCase):
-    """ Tests functions in utils module"""
+    """Tests functions in utils module"""
+
     @parameterized.expand([
-        ({"a": 1}, ["a", ], 1),
-        ({"a": {"b": 2}}, ["a", ], {"b": 2}),
-        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map, path, expected):
         """ Tests the access_nested_map func. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
-
-
-if __name__ == '__main__':
-    unittest.main()
