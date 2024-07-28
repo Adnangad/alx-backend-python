8d889a24142e1c02a993acf71d8bad514369fa5e
Adnangad <gardobuyaadnan@gmail.com>
2024-07-28 17:21:43 +0300

    5-mock a prop

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ This module tests all the methods in GithubOrgClient class."""
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 import unittest
 from client import GithubOrgClient
 from utils import get_json
@@ -19,3 +19,12 @@ class TestGithubOrgClient(unittest.TestCase):
         examp = GithubOrgClient(rez)
         examp.org()
         mock_get.assert_called_once_with(f'https://api.github.com/orgs/{rez}')
+
+
+    def test_public_repos_url(self):
+        """ Tests the case for public_repos_url method"""
+        with patch.object(GithubOrgClient, '_public_repos_url', new_callable=PropertyMock) as mock_prop:
+            mock_prop.return_value = "hello"
+            instan = GithubOrgClient("google")
+            self.assertEqual(instan._public_repos_url, "hello")
+            mock_prop.assert_called_once()
