ae9a55b4d4297ef794a7893ed73ab3c2dbc7ad56
Adnangad <gardobuyaadnan@gmail.com>
2024-07-28 13:44:09 +0300

    1-check if error is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ Tests the access_nested_map func. """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Tests if funct raises an exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
