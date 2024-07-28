8425af4e8dc6c48fc903bb53c0f9492b41c8fd69
Adnangad <gardobuyaadnan@gmail.com>
2024-07-28 12:52:49 +0300

    0-test accessed_map function

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,1 @@
+Folder contains various tests
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" This module tests all functions in the file utils. """
+from parameterized import parameterized
+import unittest
+from utils import access_nested_map
+
+
+class TestUtils(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ["a", ], 1),
+        ({"a": {"b": 2}}, ["a", ], {"b": 2}),
+        ({"a": {"b": 2}}, ["a", "b"], 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Tests the access_nested_map func. """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
